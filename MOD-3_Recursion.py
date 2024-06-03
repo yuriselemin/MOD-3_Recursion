f1c26468cfc828d18a5720c1c42ee5f808d79d16
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-06-04 00:01:54 +0300

    Update MOD-3_Recursion.py

--- MOD-3_Recursion.py
+++ MOD-3_Recursion.py
@@ -3,17 +3,17 @@
 
 
 def get_multiplied_digits(number):
-    str_number = str(number)
+  str_number = str(number)
 
-    if len(str_number) <= 1:
-        return int(str_number)
-    first = int(str_number[0])
-    remaining_number = str_number[1:]
-    return first * get_multiplied_digits(int(remaining_number))
+  if len(str_number) <= 1:
+      return int(str_number)
+  else:    
+      first = int(str_number[0])
+      remaining_number = str_number[1:]
+      return first * get_multiplied_digits(int(remaining_number))
 
 
 result = get_multiplied_digits(40203)
 print(result)
-
 
 
